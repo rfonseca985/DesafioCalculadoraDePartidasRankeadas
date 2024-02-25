0a93621740c5f683c205271b513600611c91d25d
rfonseca985 <rfonseca985@gmail.com>
2024-02-25 12:25:38 -0300

    first commit

---
+++ calcularNivel.js
@@ -0,0 +1,29 @@
+// Função para calcular o nível com base em vitórias e derrotas
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+// Exemplo de uso da função
+const resultado = calcularNivel(60, 20);
+
+// Exibição da mensagem de saída
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`);
